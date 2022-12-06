6235c2308090a795b13ed84088450ea05da123fe
ouldsaid manel <ouldsaidmanel32@gmail.com>
2022-12-06 23:51:41 +0100

    commit11

--- src/EtudiantRepository.java
+++ src/EtudiantRepository.java
@@ -5,106 +5,74 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
-
-public class EtudiantRepository {
 public class EtudiantRepository implements I_EtudiantRepository  {  
-
-
-	void add(Etudiant E) throws SQLException
+	
+	
 	public void add(Etudiant E) throws SQLException
 	{
-
-		DBConnection BD= new DBConnection();
 		DBConnection BD= DBConnection.getDBConnection(); //DBConnection -> Singleton  
 		Connection connect=BD.getConn();
-
+		
 		Statement stmt = connect.createStatement();
-		String sql = "INSERT into etudiant values (" + E.getMatricule() + ",'" + E.getNom() + "','" + E.getPrenom() + "','" + E.getEmail() + "'," +E.getNbLivreMensuel_Autorise() + "," +E.getNbLivreEmprunte() + "," +E.getId_universite()+")";
 		// J'ai ajouté E.getPwd() dans sql
 		String sql = "INSERT into etudiant values (" + E.getMatricule() + ",'" + E.getNom() + "','" + E.getPrenom() + "','" + E.getEmail() + "','" + E.getPwd() + "'," +E.getNbLivreMensuel_Autorise() + "," +E.getNbLivreEmprunte() + "," +E.getId_universite()+")";
 		int rs = stmt.executeUpdate(sql);
-
+		
 		if (rs == 1){
-@@ -25,43 +27,58 @@ void add(Etudiant E) throws SQLException
+				System.out.println("log : ajout dans la BD réussi de l'étudiant  du Matricule" + E.getMatricule());
+			}else if (rs == 0){
+				System.out.println("log : Echec de l'ajout dans la BD de l'étudiant  du Matricule" + E.getMatricule());
+			}
 		connect.close();
+		//connect.close(); C'est mieux de garder la connection ouverte (Singleton)
 	 }
 
-
-	boolean Exists(String email) throws SQLException	
 	public boolean Exists(int mat) throws SQLException	
 	{
-		DBConnection BD= new DBConnection();
 		DBConnection BD= DBConnection.getDBConnection(); //DBConnection -> Singleton  
 		Connection connect=BD.getConn();
-
+		
 		Statement stmt = connect.createStatement();
-		String sql = "select * from etudiant where email='"+ email+"'";
-		boolean rs = stmt.execute(sql);
 		String sql = "select * from etudiant where matricule=" + mat;
 		ResultSet rs = stmt.executeQuery(sql); //J'ai remplacé stmt.execute(sql);
-
-		if (rs){
-			System.out.println("logBD--- :email existe dans la BD  " + email);
-			connect.close();
-
+		
+		
 		if (rs.next()) //If ResultSet rs is not empty
 	      {       
 	        System.out.print("logBD--- :l'étudiant avec ce matricule existe déja dans la BD :\n ");
 	        System.out.format("(%d, %s, %s, %s, %s, %s)\n", rs.getInt("matricule"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"), rs.getString("pwd"), rs.getInt("nbLivreMensuel_Autorise"),rs.getInt("nbLivreEmprunte"),rs.getInt("id_universite"));
 	        connect.close();
+	      //connect.close();
 			return true;
-			}
-		System.out.println("logBD--- : email n'existe pas " + email);	
-		connect.close();
-		return false;
 	      }
 
 		else  //If ResultSet rs is empty
 		{
 			System.out.println("logBD----: l'étudiant avec ce matricule n'existe pas :" + mat);
 			connect.close();
+		 //connect.close();
 			return false;
 		}
 	}
-
-	boolean Exists(int mat) throws SQLException	
-
-
-	public boolean Exists(String email) throws SQLException	
-	{
-		DBConnection BD= new DBConnection();
-		DBConnection BD= DBConnection.getDBConnection(); //DBConnection -> Singleton  
-		Connection connect=BD.getConn();
-
-		Statement stmt = connect.createStatement();
-		String sql = "select * from etudiant where matricule="+ mat;
-		boolean rs = stmt.execute(sql);
-		String sql = "select * from etudiant where email='"+ email +"'";
-		ResultSet rs = stmt.executeQuery(sql); //J'ai remplacé stmt.execute(sql);
-
-		if (rs){
-			System.out.println("logBD--- :etudiant avec ce matricule existe déja dans la BD  " + mat);
+@@ -66,14 +66,14 @@ public boolean Exists(String email) throws SQLException
 		if (rs.next())  //If ResultSet rs is not empty
 		{
 			System.out.println("logBD--- : email existe dans la BD : " + email);
 			connect.close();
+			//connect.close(); 
 			return true;
-			}
-		System.out.println("logBD----: etudiant avec ce matricule n'existe pas " + mat);	
-		connect.close();
-		return false;
-	}
 		}
 
 		else  //If ResultSet rs is empty
 		{
 			System.out.println("logBD--- : email n'existe pas : " + email);	
 			connect.close();
+			//connect.close();
 			return false;
 		}
 
-
+		
 	}
-
-
+	
+	
 }
--- src/UniversiteRepository.java
+++ src/UniversiteRepository.java
@@ -2,29 +2,31 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-@@ -10,12 +9,12 @@ public class UniversiteRepository {
-
+import java.sql.SQLException;
+import java.sql.Statement;
+public class UniversiteRepository {
+	
+	
 	Universite GetById(int universityId) throws SQLException {
-
-		DBConnection BD= new DBConnection();
+		
 		DBConnection BD= DBConnection.getDBConnection();
 		Connection connect=BD.getConn(); 
 		Statement stmt = connect.createStatement();
 		System.out.println("LogBD : début recherche de id université dans la base de donnée");
-
-		String sql = "select * from universite where id_universite="+ universityId;
+		
 		String sql = "select * from universite where i_univ="+ universityId;
 		ResultSet rs = stmt.executeQuery(sql);
 		rs.next();	
 		TypePackage p=TypePackage.valueOf(rs.getString(3));
 		Universite u = new Universite (rs.getInt(1),rs.getString(2),p);
-			
+
 		System.out.println("LogBD : université récupérée");
-		
+
 		connect.close();
+		//connect.close(); C'est mieux de garder la connection ouverte (Singleton)
 		return u;	
-	
-		
+
+
 	}	
 	
 }
